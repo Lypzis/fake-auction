f264778cf5635d384ef763050856bb08e5e38746
Lypzis <victorvp2008@hotmail.com>
2019-07-04 17:31:01 -0300

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import AuctionBattle from './AuctionBattle/AuctionBattle'
+
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <AuctionBattle />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/AuctionBattle/AuctionBattle.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Phaser from 'phaser';
+
+import AuctionBattleScene from '../../scenes/AuctionBattleScene';
+
+class AuctionBattle extends Component {
+
+  componentDidMount() {
+    const gameConfig = {
+      parent: "aution-battle",
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 200 }
+        }
+      },
+      scene: [ AuctionBattleScene ]
+    }
+
+    new Phaser.Game(gameConfig);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="aution-battle"></div>
+        <h1>This is a test :D</h1>
+      </React.Fragment>
+      
+    );
+  }
+}
+
+export default AuctionBattle;
